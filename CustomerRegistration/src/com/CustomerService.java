9908ff69cdcbedefe66dd2d076da6eab4c4dd62b
IT20018528 <dilminiathapaththupcc@gmail.com>
2022-04-25 19:55:22 +0530

    Seventh commit

--- CustomerRegistration/src/com/CustomerService.java
+++ CustomerRegistration/src/com/CustomerService.java
@@ -14,5 +14,26 @@ import model.Customer;
 @Path("/Customer")
 public class CustomerService {
 	Customer registerObj = new Customer();
-
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readCustomer() {
+		return registerObj.readCustomer();
+	}
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertCustomer(@FormParam("cName") String cName,
+			
+	 @FormParam("cAddress") String cAddress,
+	 @FormParam("cEmail") String cEmail,
+	 @FormParam("cDate") String cDate,
+	 @FormParam("pno") String pno)
+	{
+	 String output = registerObj.insertCustomer(cName, cAddress, cEmail, cDate, pno);
+	return output;
+	}
 	
