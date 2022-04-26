5f75e8947167f23cafb2e4ac6783479df4960bef
IT20018528 <dilminiathapaththupcc@gmail.com>
2022-04-26 10:19:42 +0530

    Admin

---
+++ AdminRegistration/src/com/AdminService.java
@@ -0,0 +1,18 @@
+package com;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//For XML 
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+import model.Admin;
+
+@Path("/Admin")
+public class AdminService {
+	Admin registerObj = new Admin();
+
+	
