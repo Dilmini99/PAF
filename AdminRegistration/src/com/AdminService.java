198879bd31a11ba4e8488bbba7af293d808b7db5
IT20018528 <dilminiathapaththupcc@gmail.com>
2022-04-26 12:00:47 +0530

    sixteen commit

--- AdminRegistration/src/com/AdminService.java
+++ AdminRegistration/src/com/AdminService.java
@@ -14,5 +14,25 @@ import model.Admin;
 @Path("/Admin")
 public class AdminService {
 	Admin registerObj = new Admin();
-
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readAdmin() {
+		return registerObj.readAdmin();
+	}
+	
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertAdmin(@FormParam("cName") String cName,
+			
+	 @FormParam("cAddress") String cAddress,
+	 @FormParam("cEmail") String cEmail,
+	 @FormParam("cDate") String cDate,
+	 @FormParam("pno") String pno)
+	{
+	 String output = registerObj.insertAdmin(cName, cAddress, cEmail, cDate, pno);
+	return output;
+	}
 	
